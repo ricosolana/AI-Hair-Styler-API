80ef43e5b543363d2fee1733810ece61ff96ef18
PeriodicSeizures <47781580+PeriodicSeizures@users.noreply.github.com>
2024-03-22 19:29:59 -0400

    simplify svoji code

--- svoji.py
+++ svoji.py
@@ -41,121 +41,53 @@ bottom_right_x, bottom_right_y = 500, 350
 
 
 def process_svoji(image):
-    start_time = time.time()
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    image = cv2.resize(image, (800, 400))
-    image1 = image
-    image1.flags.writeable = False
+    results = face_mesh.process(image)
 
-    results = face_mesh.process(image1)
-
-    image1.flags.writeable = True
-
-    img_h, img_w, img_c = image1.shape
-    face_3d = []
-    face_2d = []
-
-    # Draw a rectangle on the image with a dot in the center
-    cv2.rectangle(image1, (top_left_x, top_left_y), (bottom_right_x, bottom_right_y), (255, 0, 0), 2)
-    center_x = int((top_left_x + bottom_right_x) / 2)
-    center_y = int((top_left_y + bottom_right_y) / 2)
-    # cv2.circle(image1, (center_x, center_y), 5, (0, 255, 0), -1)
-
-    #recognize_pose = False
+    img_h, img_w, img_c = image.shape
 
     if results.multi_face_landmarks:
         for face_landmarks in results.multi_face_landmarks:
-            nose_x = int(face_landmarks.landmark[1].x * img_w)
-            nose_y = int(face_landmarks.landmark[1].y * img_h)
+            nose_top = face_landmarks.landmark[195]
+            nose_mid = face_landmarks.landmark[1]
+            nose_bot = face_landmarks.landmark[2]
 
-            # If the nose is in the center of the rectangle, initiate pose recognition and image capture
-            if top_left_x < nose_x < bottom_right_x and top_left_y < nose_y < bottom_right_y:
-                recognize_pose = True
-            else:
-                recognize_pose = False
+            right_eye1 = face_landmarks.landmark[33]
+            right_eye2 = face_landmarks.landmark[133]
 
-            # Pose recognition and image capture
-            if recognize_pose:
-                face_center_x = int((face_landmarks.landmark[33].x + face_landmarks.landmark[263].x) * img_w / 2)
-                face_center_y = int((face_landmarks.landmark[33].y + face_landmarks.landmark[263].y) * img_h / 2)
+            left_eye1 = face_landmarks.landmark[263]
+            left_eye2 = face_landmarks.landmark[362]
 
-                # If the face is within the rectangular region, capture images for different poses
-                if top_left_x < face_center_x < bottom_right_x and top_left_y < face_center_y < bottom_right_y:
-                    # instead of decomposing points to 3d, draw 2d?
-                    # this will be rawly-rastered; the perspective will be baked, pose-as-is
-                    # maybe later add a rotation / normalizing feature
+            d = drawsvg.Drawing(800, 400)
 
+            # Nose
+            p = drawsvg.Path(stroke_width=2, stroke='lime', fill='black', fill_opacity=0.2)
+            p.M(nose_top.x * img_w, nose_top.y * img_h)  # Start path at point (nose_top)
+            #p.C(30, 10, 30, -50, 70, -20)  # Draw a curve to (70, -20)
+            p.L(nose_mid.x * img_w, nose_mid.y * img_h)
+            p.L(nose_bot.x * img_w, nose_bot.y * img_h)
 
+            # Right eye
+            p.M(right_eye1.x * img_w, right_eye1.y * img_h)
+            p.L(right_eye2.x * img_w, right_eye2.y * img_h)
 
-                    #d.save_svg(fname='tmp1.svg')
+            # Left eye
+            p.M(left_eye1.x * img_w, left_eye1.y * img_h)
+            p.L(left_eye2.x * img_w, left_eye2.y * img_h)
 
-                    # nose
-                    #   195(4, 5? top) none exact
-                    #   1(tip)
-                    #   2(bottom)
+            d.append(p)
 
-                    nose_top = face_landmarks.landmark[195]
-                    nose_mid = face_landmarks.landmark[1]
-                    nose_bot = face_landmarks.landmark[2]
+            png_io = BytesIO()
+            d.save_png(png_io)
+            png_io.seek(0)
 
-                    right_eye1 = face_landmarks.landmark[33]
-                    right_eye2 = face_landmarks.landmark[133]
+            png_bytes = np.asarray(bytearray(png_io.read()), dtype=np.uint8)
+            conv_image = cv2.imdecode(png_bytes, cv2.IMREAD_COLOR)
 
-                    left_eye1 = face_landmarks.landmark[263]
-                    left_eye2 = face_landmarks.landmark[362]
+            return conv_image
 
-                    d = drawsvg.Drawing(800, 400) #, origin='center')
-
-                    # Nose
-                    p = drawsvg.Path(stroke_width=2, stroke='lime', fill='black', fill_opacity=0.2)
-                    p.M(nose_top.x * img_w, nose_top.y * img_h)  # Start path at point (nose_top)
-                    #p.C(30, 10, 30, -50, 70, -20)  # Draw a curve to (70, -20)
-                    p.L(nose_mid.x * img_w, nose_mid.y * img_h)
-                    p.L(nose_bot.x * img_w, nose_bot.y * img_h)
-
-                    # Right eye
-                    p.M(right_eye1.x * img_w, right_eye1.y * img_h)
-                    p.L(right_eye2.x * img_w, right_eye2.y * img_h)
-
-                    # Left eye
-                    p.M(left_eye1.x * img_w, left_eye1.y * img_h)
-                    p.L(left_eye2.x * img_w, left_eye2.y * img_h)
-
-                    d.append(p)
-
-                    png_io = BytesIO()
-                    d.save_png(png_io)
-                    png_io.seek(0)
-
-                    png_bytes = np.asarray(bytearray(png_io.read()), dtype=np.uint8)
-                    conv_image = cv2.imdecode(png_bytes, cv2.IMREAD_COLOR)
-
-                    return {
-
-                    }, conv_image
-
-                    #d.save_svg(fname='tmp2.svg')
-
-
-
-                    """
-                    if success:
-                        return {
-                            'pose': current_pose,
-                            'x_angle': x_angle,
-                            'y_angle': y_angle,
-                            'z_angle': z_angle,
-                            #'frame': enc,
-                            'time': processing_time
-                        }, enc.tobytes()
-                    else:
-                        return {'message': 'Image encoding failure'}
-                    """
-    else:
-        return {'message': 'No detectable landmarks'}
-
-    return {'message': 'Landmark is not utilized'}
+    return None
 
 
 # test
@@ -165,15 +97,15 @@ cap = cv2.VideoCapture(0)
 while cap.isOpened():
     success, image = cap.read()
 
-    #image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
     image = cv2.resize(image, (800, 400))
 
     cv2.imshow('Capture', image)
 
-    data = process_svoji(image)
+    conv_image = process_svoji(image)
 
-    if data[1] is not None:
-        cv2.imshow('Converted', data[1])
+    if conv_image is not None:
+        cv2.imshow('Converted', conv_image)
 
     if cv2.waitKey(1) == 27:
         break  # esc to quit

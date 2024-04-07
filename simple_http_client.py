0bcd6a4144ac292e0eeedc5a7f4b7f7313f4899f
PeriodicSeizures <47781580+PeriodicSeizures@users.noreply.github.com>
2024-04-07 02:56:44 -0400

    skip tls

--- simple_http_client.py
+++ simple_http_client.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 
 def test_api_pose():
@@ -29,9 +30,12 @@ def test_api_svoji():
 
 
 def test_api_barber(img_file):
-    access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMjM4NDQ2OCwianRpIjoiMThiYTcxZjAtYTA3Ny00MzRhLWIxNjktMjBkYzhhZDkwMThhIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImFub255bW91cyIsIm5iZiI6MTcxMjM4NDQ2OCwiY3NyZiI6Ijg5MzBmMDMwLWQyYzUtNDA4MS04Nzk5LWRkZGIwNjY5NWNkOCIsImV4cCI6MTcxMjM4NTM2OH0.XThOc1amUcMDEiectSh3sZcYNsvrt-TqaZIKtEFHf-I'
-    #url = 'https://localhost/api/barber'
-    url = 'http://localhost/api/barber'
+    #os.environ["REQUESTS_CA_BUNDLE"] = 'C:\\Users\\rico\\Documents\\GitHub\\AI-Hair-Styler-API\\certs\\ca-cert.pem'
+    #os.environ["SSL_CERT_FILE"] = 'C:\\Users\\rico\\Documents\\GitHub\\AI-Hair-Styler-API\\certs\\ca-cert.pem'
+
+    access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMjQ1NDk3OSwianRpIjoiOWUyMjkyZGMtMzM0OC00MDVhLThkZTQtNWFhNDg4YmVmOGYyIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImFub255bW91cyIsIm5iZiI6MTcxMjQ1NDk3OSwiY3NyZiI6IjNjNTgwZmYyLTJiZmEtNDRlZS1iNWJhLTFlNzUxMTg3MzkwZiIsImV4cCI6MTcxMjQ1NTg3OX0.XmkuVZmbKvoautbTq1ez8Ti_PrEuozMBp5HEiFElAG8'
+    url = 'https://localhost/api/barber'
+    #url = 'http://localhost/api/barber'
 
     headers = {
         'Authorization': f'Bearer {access_token}'
@@ -47,7 +51,11 @@ def test_api_barber(img_file):
     response = requests.post(url,
                              headers=headers,
                              files=files,
-                             params=query_params)
+                             params=query_params,
+                             #verify='C:\\Users\\rico\\Downloads\\O=periodicseizures,L=Tampa,ST=Florida,C=US.crt'
+                             #verify='C:\\Users\\rico\\Downloads\\aihairstyler.crt'
+                             verify=False
+                             )
 
     print(response.json())
 
